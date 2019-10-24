7cf462c604a0dbbddb64e918945336bab1fcf678
PapuItalo <PapuItalo@Italoore100>
2019-10-23 23:11:28 -0500

    Compilando detalles de factura

--- TrabajoFinal_Cinemark/src/main/java/pe/edu/upc/repository/Detalle_List_CompraRepository.java
+++ TrabajoFinal_Cinemark/src/main/java/pe/edu/upc/repository/Detalle_List_CompraRepository.java
@@ -2,15 +2,15 @@ package pe.edu.upc.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.entity.Detalle_List_Compra;
 
 @Repository
 public interface Detalle_List_CompraRepository extends JpaRepository<Detalle_List_Compra, Integer>{
-	@Query("select d from Detalle d where d.unidadesDetalle = ?1")
+	@Query("select d from Detalle_List_Compra d where d.unidadesDetalle = ?1")
 	List<Detalle_List_Compra> findByUnidadesDetalle (int unidadesDetalle);
 }
